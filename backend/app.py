8d5235360ccceceaf3e2b67b6782a428179283e6
olmaximova <ol.maximova1309@gmail.com>
2026-04-25 13:06:24 +0300

    feat: add /analyze route

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder="../frontend")
+app = Flask(__name__, static_folder="../frontend", static_url_path="" )
 
 @app.route("/")
 def index():
@@ -12,6 +12,10 @@ def upload():
 
 @app.route("/train")
 def train_model():
+    pass
+
+@app.route("/analyze")
+def analyze_data():
     pass
 
 @app.route("/visuals")
